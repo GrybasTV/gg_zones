42e3da8588fc032e7f5d831be4c0fd7e9bccfef9
GrybasTV <118368718+GrybasTV@users.noreply.github.com>
2024-06-26 15:03:55 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,29 +6,15 @@ local blips = {}
 function CreateZoneBlip(zone)
     Citizen.CreateThread(function()
         -- Sukuriamas blip'as
-        local blip = Citizen.InvokeNative(0x45F13B7E0A15C880, zone.blipId, zone.x, zone.y, zone.z, zone.radius)
-        table.insert(blips, blip) -- Pridedame blip'ą į registrą
+        local blip = Citizen.InvokeNative(0x45F13B7E0A15C880, zone.blipId, zone.x, zone.y, zone.z, zone.radius)      
 
         -- Palaukite 45 sekundes
-        Citizen.Wait(45000)
+        -- Citizen.Wait(45000)
 
         -- Pašalina blip'ą
-        Citizen.InvokeNative(0x45FF974EEE1C8734, blip)
+        -- Citizen.InvokeNative(0x45FF974EEE1C8734, blip)
     end)
 end
-
--- debug: Tikriname, ar Config teisingai įkrautas
-Citizen.CreateThread(function()
-    if not Config or not Config.BlipZones then
-        print("Config arba Config.BlipZones yra nil")
-        return
-    end
-
-    for i, zone in ipairs(Config.BlipZones) do
-        print("Sukuriamas blip'as zonoje:", i, zone.x, zone.y, zone.z)
-        CreateZoneBlip(zone)
-    end
-end)
 
 -- Funkcija, pašalinanti visus sukurtus blip'us
 function RemoveAllBlips()
